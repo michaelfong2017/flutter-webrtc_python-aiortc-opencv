747a2b33a172d7e123b8a8fa3932a8beac28a5e1
michaelfong2017 <michaelfong2017@gmail.com>
2022-02-14 04:55:12 +0800

    Critical changes to detector.py so that frames are not skipped and bounding boxes are retained before the next object detection result is available.

--- server/detector.py
+++ server/detector.py
@@ -22,8 +22,13 @@ class Detector:
         self.img = None
         self.is_processing = False
 
+        self.classes = None
+        self.confidences = None
+        self.boxes = None
+
     def detect(self, img):
         if self.is_processing:
+            _draw(self.img, self.names, self.classes, self.confidences, self.boxes)
             return
 
         self.is_processing = True
@@ -34,21 +39,34 @@ class Detector:
     
     @timeit
     def _detect(self, img):
-        classes, confidences, boxes = self.net.detect(img, confThreshold=0.1, nmsThreshold=0.4)
-        if len(confidences) > 0:
-            for classId, confidence, box in zip(classes.flatten(), confidences.flatten(), boxes):
-                label = '%s: %.2f' % (self.names[classId], confidence)
-                labelSize, baseLine = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.5, 1)
-                left, top, width, height = box
-                top = max(top, labelSize[1])
-                # b = random.randint(0, 255)
-                # g = random.randint(0, 255)
-                # r = random.randint(0, 255)
-                b = 0
-                g = 0
-                r = 255
-                cv2.rectangle(img, box, color=(b, g, r), thickness=2)
-                cv2.rectangle(img, (left - 1, top - labelSize[1]), (left + labelSize[0], top), (b, g, r), cv2.FILLED)
-                cv2.putText(img, label, (left, top), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255 - b, 255 - g, 255 - r))
+        # It is critical to use deep copy here because the "img" argument of "_draw"
+        # is passed by reference.
+        self.img = img.copy()
+        _draw(self.img, self.names, self.classes, self.confidences, self.boxes)
 
-        self.img = img
+        classes, confidences, boxes = self.net.detect(img, confThreshold=0.1, nmsThreshold=0.4)
+        self.classes = classes
+        self.confidences = confidences
+        self.boxes = boxes
+
+        # Draw to the latest img instead of the img that is originally analyzed.
+        # Therefore, use self.img instead of img as the argument.
+        _draw(self.img, self.names, classes, confidences, boxes)
+
+
+def _draw(img, names, classes, confidences, boxes):
+    if confidences is not None and len(confidences) > 0:
+        for classId, confidence, box in zip(classes.flatten(), confidences.flatten(), boxes):
+            label = '%s: %.2f' % (names[classId], confidence)
+            labelSize, baseLine = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.5, 1)
+            left, top, width, height = box
+            top = max(top, labelSize[1])
+            # b = random.randint(0, 255)
+            # g = random.randint(0, 255)
+            # r = random.randint(0, 255)
+            b = 0
+            g = 0
+            r = 255
+            cv2.rectangle(img, box, color=(b, g, r), thickness=2)
+            cv2.rectangle(img, (left - 1, top - labelSize[1]), (left + labelSize[0], top), (b, g, r), cv2.FILLED)
+            cv2.putText(img, label, (left, top), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255 - b, 255 - g, 255 - r))
